a7d0c849ddf3925c234bc763ee91ff58126a11f3
Tadao Iseki <saitoeku3@gmail.com>
2020-03-18 23:22:30 +0900

    local version

--- src/server/src/storage/storage.service.ts
+++ src/server/src/storage/storage.service.ts
@@ -1,24 +1,17 @@
 import { Injectable } from '@nestjs/common'
-import * as AWS from 'aws-sdk'
 import { FileUpload } from 'graphql-upload'
-
-const { AWS_REGION, AWS_S3_BUCKET_NAME } = process.env
-
-AWS.config.update({ region: AWS_REGION })
+import { createWriteStream } from 'fs'
+import { resolve } from 'path'
 
 @Injectable()
 export class StorageService {
-  private s3: AWS.S3
-
-  constructor() {
-    this.s3 = new AWS.S3({ apiVersion: '2006-03-01' })
-  }
-
   async upload({ createReadStream, filename }: FileUpload): Promise<string> {
-    const stream = createReadStream()
-    const result = await this.s3
-      .upload({ Bucket: AWS_S3_BUCKET_NAME!, Key: filename, Body: stream })
-      .promise()
-    return result.Location
+    const readSteam = createReadStream()
+    const location = resolve('upload', filename)
+    const writeStream = createWriteStream(location, 'utf8')
+
+    readSteam.pipe(writeStream)
+
+    return location
   }
 }
